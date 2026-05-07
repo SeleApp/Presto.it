3cc6b86d98374d42e333528f0e2c4acb55c076c3
AleVecia <seleapp.info@gmail.com>
2026-05-07 23:26:22 +0200

    User Story 1 completata

---
+++ presto_cognome1_cognome2/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PublicController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
+Route::get('/articles/create', [ArticleController::class, 'create'])->middleware('auth')->name('articles.create');
